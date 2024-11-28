606d76e9834022acc11477ec6f987ee07e3d2614
layla-reiss <sapphirelayla2005@gmail.com>
2024-11-28 13:54:22 +0000

    constructed Player1.cs

--- lyra-astractus/GeneralClasses/Player1.cs
+++ lyra-astractus/GeneralClasses/Player1.cs
@@ -2,9 +2,23 @@ namespace ConsoleApp1.GeneralClasses;
 
 public class Player1
 {
-    //TODO inventory, health metrics, skills, quests
-    //TODO method for freezing
+    internal int maxHealth;
+    internal int remainingHealth;
+    private quest[] questLog { get; private set; }; //TODO make quest class
+    private skill[] skillList { get; private set; }; //TODO make skill class
+    private posession[] inventory { get; private set; }; //TODO make posession class
+ //TODO method for freezing
     //TODO method for suffocation
     //TODO method for overheating
     //TODO method for taking damage
+    public Player1(int maxHealth, int remainingHealth, quest[] questLog, skill[] skillList, posession[] inventory)
+    {
+        this.maxHealth = maxHealth;
+        this.remainingHealth = remainingHealth;
+        this.questLog = questLog;
+        this.skillList = skillList;
+        this.inventory = inventory;
+    }
+    
+    
 }
