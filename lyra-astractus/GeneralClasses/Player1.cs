dbe04dd933c041b68b2b20db8e59fe67af9bef1f
layla-reiss <sapphirelayla2005@gmail.com>
2024-11-28 13:25:40 +0000

    added GuardConditions.cs attributes and plan for Player1.cs attributes and methods

--- lyra-astractus/GeneralClasses/Player1.cs
+++ lyra-astractus/GeneralClasses/Player1.cs
@@ -3,4 +3,8 @@ namespace ConsoleApp1.GeneralClasses;
 public class Player1
 {
     //TODO inventory, health metrics, skills, quests
+    //TODO method for freezing
+    //TODO method for suffocation
+    //TODO method for overheating
+    //TODO method for taking damage
 }
--- lyra-astractus/Guard/GuardConditions.cs
+++ lyra-astractus/Guard/GuardConditions.cs
@@ -2,5 +2,14 @@ namespace ConsoleApp1.Guard;
 
 public interface GuardConditions
 {
-    //TODO temperature, gravity
+    private int temp = 5;
+    
+    //make gravity class with physics methods for each which can be applied later at the FE
+    private string gravity = "earth";
+
+    //gas masks needed from smog
+    private boolean breathableAir = false;
+
+    //demilitarised zone, eathlight fellowship controls weaponry
+    private boolean gunsProhibited = false;
 }
